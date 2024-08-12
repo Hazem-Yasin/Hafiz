4943a8cd47fb3bdd428a2af8a3a9bf97fdd6c040
Hazem Ya-sin <hazem.yasin.1416@gmail.com>
2024-08-12 19:40:47 +0300

    added some stuff

---
+++ frontend/hafizTS/hafizts/src/Components/SurahComp/SurahComponent.tsx
@@ -0,0 +1,10 @@
+import surahDB from "../Components/SurahComp/SurahDB.json"
+function SurahComponent() {
+  return (
+      //<h1>{surahDB[0].surahName}</h1>
+      surahDB
+
+  );
+}
+
+export default SurahComponent;
--- frontend/hafizTS/hafizts/src/main.tsx
+++ frontend/hafizTS/hafizts/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactComponent from './ReactComponent.tsx'
+import SurahComponent from './Components/SurahComp/SurahComponent.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ReactComponent />
+        <SurahComponent />
+    </React.StrictMode>,
 )
